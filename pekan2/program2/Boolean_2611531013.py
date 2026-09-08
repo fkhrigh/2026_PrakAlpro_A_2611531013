844263ed537c008294efbdd3db96442e379b41e5
fkhrigh <fakhrighifari24@gmail.com>
2026-09-08 21:54:37 +0700

    Add files via upload

---
+++ pekan2/program2/Boolean_2611531013.py
@@ -0,0 +1,17 @@
+# Nama variable ditambah 4 digit nim terakhir contoh: nilai_1234
+# Deklarasi variable dengan tipe data Boolean
+is_lulus_1013 = True
+is_cumlaude_1013 = True
+
+# Menggunakan Boolean
+nilai_1013 = 85
+batas_lulus_1013 = 75
+
+# Menentukan nilai Boolean dari kondisi 
+status_kelulusan_1013 = nilai_1013 >= batas_lulus_1013 #Hasilnya akan True
+
+print("=== Check Kelulusan ===")
+print("Nilai:", nilai_1013)
+print("Apakah Lulus?:", status_kelulusan_1013)
+if is_lulus_1013 and is_cumlaude_1013:
+    print("Selamat, Anda lulus dengan predikat Cum Laude!")
---
+++ pekan2/program2/Input_2611531013.py
@@ -0,0 +1,11 @@
+# Buat program untuk menghitung luas persegi panjang
+# Nama variabel ditambah 4 digit nim terakhir contoh: panjang 1234
+# Program ini menggunakan fungsi input() untuk meminta pengguna
+# Memasukkan nilai panjang dan lebar persegi panjang.
+# Nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+
+panjang_1013 = input('Masukkan nilai panjang: ')
+lebar_1013 = input('Masukkan nilai lebar: ')
+
+luas_1013 = int(panjang_1013) * int(lebar_1013)
+print("Luas = ", luas_1013)
