a68c0ae5b4acb36b67b86bb7b29a7d1e7fc50f36
fkhrigh <fakhrighifari24@gmail.com>
2026-09-10 20:23:12 +0700

    perbaiki laprak

--- pekan1/program1/program1.py
+++ pekan1/program1/program1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #program pertama 
 """
 komentar
@@ -11,18 +10,4 @@ print("Program ini  menghasilkan")
 print("Empat baris output")
 print("/ \\ // \\\\ /// \\\\\\")
 print("program ini" + "dibuat oleh a\n"
-=======
-#program pertama 
-"""
-komentar
-dua baris atau lebih 
-\pertemuan 1 praktikum alpro
-"""
-print("Hello World")
-print()
-print("Program ini  menghasilkan")
-print("Empat baris output")
-print("/ \\ // \\\\ /// \\\\\\")
-print("program ini" + "dibuat oleh a\n"
->>>>>>> d6afa4043101782e980e2489c7e034d529665c13
 + "Fakhri Al Ghifari Busra dan 2611531013")
--- pekan2/program2/String_2611531013.py
+++ pekan2/program2/String_2611531013.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
 # Ganti programnya dan data masing masing
 
@@ -14,21 +13,4 @@ agama_1013 = "Islam"
 print("Nama saya adalah: ", nama_1013)
 print("Jenis kelamin saya adalah: ", jenis_kelamin_1013)
 print("Alamat saya adalah: ", alamat_1013)
-=======
-# Nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
-# Ganti programnya dan data masing masing
-
-nama_1013 = "Fakhri Al Ghifari Busra"
-jenis_kelamin_1013 = 'L'
-alamat_1013 = """
-    Koto Panjang
-    Kecamatan Pauh
-    Kota Padang
-"""
-agama_1013 = "Islam"
-
-print("Nama saya adalah: ", nama_1013)
-print("Jenis kelamin saya adalah: ", jenis_kelamin_1013)
-print("Alamat saya adalah: ", alamat_1013)
->>>>>>> d6afa4043101782e980e2489c7e034d529665c13
 print("Agama saya adalah: ", agama_1013)
